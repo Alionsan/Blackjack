26c5b50000b3e559bbda4f145acad2b2cef98a50
Raul Osmar García Cruz <raul.garcia.cruz@uabc.edu.mx>
2026-09-17 15:24:19 -0700

    Eliminar carpeta obsoleta

--- main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.clase.solitario { // El nombre puede variar
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    // Permite que JavaFX use tus paquetes
-    exports solitaire;
-    exports DeckOfCards;
-}
