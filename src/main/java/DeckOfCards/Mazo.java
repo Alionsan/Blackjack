24232e85913b986ba421aacb29d9921e2a37de1f
Raul Osmar García Cruz <raul.garcia.cruz@uabc.edu.mx>
2026-09-18 00:56:53 -0700

    Sustitución de ArrayList por una pila en la clase Mazo

--- src/main/java/DeckOfCards/Mazo.java
+++ src/main/java/DeckOfCards/Mazo.java
@@ -7,13 +7,47 @@ package DeckOfCards;
  */
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
+
+import pila.Pila;
 
 public class Mazo {
-    private ArrayList<CartaInglesa> cartas = new ArrayList<>();
+    private Pila<CartaInglesa> cartas;
 
     public Mazo() {
-        llenar(); // crea todas las cartas, excluyendo Jokers
-        mezclar();
+        cartas = new Pila<>(52);
+
+    }
+
+    public CartaInglesa obtenerUnaCarta(){
+        if (!cartas.pilaVacia()){
+            return cartas.pop();
+        }
+        return null;
+    }
+
+    private void llenarYMezclar(){
+        CartaInglesa[] arregloTemporal = new CartaInglesa[52];
+        int indice = 0;
+
+        for(int i = 2; i<= 14; i++){
+            for(Palo palo : Palo.values()){
+                arregloTemporal[indice] = new CartaInglesa(i, palo, palo.getColor());
+                indice++;
+            }
+        }
+
+        Random rand = new Random();
+        for(int i = arregloTemporal.length - 1; i > 0; i--){
+            int j = rand.nextInt(i+1);
+            CartaInglesa temp = arregloTemporal[i];
+            arregloTemporal[i] = arregloTemporal[j];
+            arregloTemporal[j] = temp;
+        }
+
+        for(CartaInglesa c : arregloTemporal){
+            cartas.push(c);
+        }
     }
 
     /**
@@ -24,12 +58,6 @@ public class Mazo {
         return cartas;
     }
 
-    public CartaInglesa obtenerUnaCarta() {
-        if (cartas.size() > 0) {
-            return cartas.remove(0);
-        }
-        return null;
-    }
     private void mezclar() {
         Collections.shuffle(cartas);
     }
